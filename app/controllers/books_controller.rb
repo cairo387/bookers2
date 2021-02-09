d67273972a4cec18284185c8ff2e27d2df0f7d91
cairo387 <mifuneri@outlook.jp>
2021-02-09 09:38:11 +0000

    Teritary Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all.order(id: :asc)
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all.order(id: :asc)
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.title = params[:title]
+    @book.body = params[:body]
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render ("books/edit")
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
